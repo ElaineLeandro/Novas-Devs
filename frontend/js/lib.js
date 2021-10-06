971763a585f9d514a4b8a56840c5360f91c9deda
Amanda Bonafé <89615157+amybonafe@users.noreply.github.com>
2021-10-05 22:54:56 -0300

    testedeapi

--- frontend/js/lib.js
+++ frontend/js/lib.js
@@ -1,13 +1,27 @@
 function save (){
-    var nome = $("#nome").val();
-    alert(nome);
     
+    var nome = $("#nome").val();
+    alert(nome);    
+    alert("ola mamae");
     $.get("../../backend/indexmock.txt",
     function(data){
         $("#conteudo").html(data);
     }
     )
 } 
+function mostraPokemon(){
+    $.ajax({
+        url: "https://pokeapi.co/api/v2/pokemon-form/12/"
+      }).done(function(resultado) {
+        alert(resultado.name + resultado.sprites.back_default);
+        console.log(resultado);
+        $('#sprite').html("<img src='"+resultado.sprites.back_default+"'>");             
+      }).fail(function(jqXHR, textStatus, msg){
+        alert("erro ao realizar requisição");
+   }); 
+}
+
+
 function insereLinhaTabela(){
     tabela= document.getElementById("minhaTabela");
     novaLinha = tabela.insertRow();
